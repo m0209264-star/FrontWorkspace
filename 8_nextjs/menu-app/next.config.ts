8c97e41bf73eecd3c2e987b180d16456aa7d332c
DESKTOP-CJQ195S\user1 <m0209264@gmail.com>
2026-04-29 12:42:56 +0900

    12

--- 8_nextjs/menu-app/next.config.ts
+++ 8_nextjs/menu-app/next.config.ts
@@ -2,6 +2,34 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        protocol:'http',
+        hostname:'img1.kakaocdn.net',
+        port:'',
+        pathname:'/**'
+      },
+      {
+        protocol:'https',
+        hostname:'img1.kakaocdn.net',
+        port:'',
+        pathname:'/**'
+      },
+      {
+        protocol:'http',
+        hostname:'k.kakaocdn.net',
+        port:'',
+        pathname:'/**'
+      },
+      {
+        protocol:'https',
+        hostname:'k.kakaocdn.net',
+        port:'',
+        pathname:'/**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
